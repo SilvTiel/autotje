49ddfa456fda9fbd4aa9297a4587be0d75659966
Sil van Tiel <silvantiel@gmail.com>
2021-09-27 11:12:23 +0200

    versie 1.0

--- main.js
+++ main.js
@@ -2,6 +2,18 @@ let log = console.log;
 let beweging = 20;
 let auto = document.querySelector('.auto');
 
+document.addEventListener('DOMContentLoaded', init);
+
+function init(){
+    document.body.addEventListener('keydown', anyKey);
+}
+
+function anyKey(ev){
+    // log(ev);
+    let char = ev.which;
+    log(char);
+}
+
 window.addEventListener('load', () => {
     auto.style.position = 'absolute';
     auto.style.left = 0;
@@ -11,29 +23,48 @@ window.addEventListener('load', () => {
 window.addEventListener('keydown' , (e) => {
 
     switch (e.key) {
-        case 'ArrowUp': //W
+        case 'ArrowUp':
+        case 'w':
             auto.style.top = parseInt(auto.style.top) - beweging + 'px';
+            auto.style.transform = 'rotate(0deg)';
             break;
-        case 'ArrowRight': //D
+        case 'ArrowRight':
+        case'd':
             auto.style.left = parseInt(auto.style.left) + beweging + 'px';
+            auto.style.transform = 'rotate(90deg)';
             break;
-        case 'ArrowDown'://S
+        case 'ArrowDown':
+        case's':
             auto.style.top = parseInt(auto.style.top) + beweging + 'px';
+            auto.style.transform = 'rotate(180deg)';
             break;
-        case 'ArrowLeft'://A
+        case 'ArrowLeft':
+        case'a':
             auto.style.left = parseInt(auto.style.left) - beweging + 'px';
+            auto.style.transform = 'rotate(270deg)';
+            break;
+        case 'h':
+            play();
+            break;
+        case 'i':
+            verrandering();
+            break;
+        case 'o':
+            color();
             break;
 
     }
 })
-// document.addEventListener('DOMContentLoaded', init);
 
-// function init(){
-//     document.body.addEventListener('keydown', anyKey);
-// }
+function play() {
+    var audio = new Audio('meep-meep.wav');
+    audio.play();
+  }
 
-// function anyKey(ev){
-//     log(ev);
-//     let char = ev.which;
-//     log(char);
-// }
+  function verrandering(){
+          auto.style.backgroundImage = "url('afbeeldingen/car-top.svg')"
+  }
+
+  function color(){
+    auto.style.backgroundImage = "url('afbeeldingen/car.png')"
+  }
